783184849833d944ede110950b1d40bab9f0e8e9
Jörg Steffen <steffen@dfki.de>
2020-09-21 17:16:35 +0200

    Add cache to Marian NMT client

--- src/main/java/de/dfki/mlt/transins/MarianNmtClient.java
+++ src/main/java/de/dfki/mlt/transins/MarianNmtClient.java
@@ -3,6 +3,9 @@ package de.dfki.mlt.transins;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.WebSocket;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
@@ -19,12 +22,17 @@ public class MarianNmtClient implements WebSocket.Listener {
 
   private static final Logger logger = LoggerFactory.getLogger(MarianNmtClient.class);
 
+  // number of translation to keep in cache
+  private static final int TRANSLATION_CACHE_SIZE = 100;
+
   // the underlying socket
   private WebSocket ws;
   // the completable future holding the server's translation result
   private CompletableFuture<String> responseFuture;
   // here we build the server's translation result from potentially multiple server responses
   private StringBuilder serverResponse;
+  // translation cache
+  private Map<String, String> translationCache;
 
 
   /**
@@ -40,6 +48,18 @@ public class MarianNmtClient implements WebSocket.Listener {
         .newWebSocketBuilder()
         .buildAsync(URI.create(url), this)
         .join();
+
+    // init translation cache
+    this.translationCache =
+        Collections.synchronizedMap(
+            new LinkedHashMap<String, String>(TRANSLATION_CACHE_SIZE + 1, .75F, true) {
+
+              @Override
+              public boolean removeEldestEntry(Map.Entry<String, String> eldest) {
+
+                return size() > TRANSLATION_CACHE_SIZE;
+              }
+            });
   }
 
 
@@ -75,12 +95,20 @@ public class MarianNmtClient implements WebSocket.Listener {
   public String send(String query)
       throws InterruptedException, ExecutionException {
 
+    // use cached translation if available
+    String cachedTranslation = this.translationCache.get(query);
+    if (cachedTranslation != null) {
+      return cachedTranslation;
+    }
+
     // create new completable future that will hold the translation result
     this.responseFuture = new CompletableFuture<String>();
     this.serverResponse = new StringBuilder();
     this.ws.sendText(query, true);
     // this call blocks until the translation is available
-    return this.responseFuture.get();
+    String translation = this.responseFuture.get();
+    this.translationCache.put(query, translation);
+    return translation;
   }
 
 
