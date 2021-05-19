ee60acfb9dfcb66c5a684aca48e0c622fb89ee73
Jörg Steffen <steffen@dfki.de>
2021-05-19 15:18:11 +0200

    Run docker commands with sudo

--- build-transins-docker-images.sh
+++ build-transins-docker-images.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export DOCKER_BUILDKIT=1
-docker build -t dfki/transins-prepost:0.0.1 -f Dockerfile-PrePostprocessing .
-docker build -t dfki/transins-marian:0.0.1 -f Dockerfile-MarianNMT .
-docker build -t dfki/transins-server:0.0.1 -f Dockerfile-Server .
+sudo -- sh -c 'export DOCKER_BUILDKIT=1; \
+  docker build -t dfki/transins-prepost:0.0.1 -f Dockerfile-PrePostprocessing . ; \
+  docker build -t dfki/transins-marian:0.0.1 -f Dockerfile-MarianNMT . ; \
+  docker build -t dfki/transins-server:0.0.1 -f Dockerfile-Server .'
