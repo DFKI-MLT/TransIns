f315db47a9e9d50149100cd3d5155de49bb4593b
Jörg Steffen <steffen@dfki.de>
2021-06-28 15:23:02 +0200

    Bump version to 1.0.0

--- build-transins-docker-images.sh
+++ build-transins-docker-images.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo -- sh -c 'export DOCKER_BUILDKIT=1; \
-  docker build -t dfki/transins-prepost:0.0.1 -f Dockerfile-PrePostprocessing . ; \
-  docker build -t dfki/transins-marian:0.0.1 -f Dockerfile-MarianNMT . ; \
-  docker build -t dfki/transins-server:0.0.1 -f Dockerfile-Server .'
+  docker build -t dfki/transins-prepost:1.0.0 -f Dockerfile-PrePostprocessing . ; \
+  docker build -t dfki/transins-marian:1.0.0 -f Dockerfile-MarianNMT . ; \
+  docker build -t dfki/transins-server:1.0.0 -f Dockerfile-Server .'
